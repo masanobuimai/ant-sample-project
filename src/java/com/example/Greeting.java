486404f79b2732681628b95fee8aa302f19e54e3
masanobu.imai <masanobu.imai@gmail.com>
2012-04-05 10:12:13 +0900

    「Jenkinsではじめるビルド職人入門」の最初の状態

---
+++ src/java/com/example/Greeting.java
@@ -0,0 +1,8 @@
+package com.example;
+
+
+public class Greeting {
+    public String sayHello() {
+        return "Hello World!!";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,2 @@
+<jsp:useBean id="greeting" class="com.example.Greeting"/>
+<h1><%= greeting.sayHello() %></h1>
